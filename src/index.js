6bc2b755cecbf2b7b4bb5034becd56f16ea3bd5b
Delete3 <mjian827@gmail.com>
2021-06-30 18:54:36 +0800

    跟著網路做到一半

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+
+const DATA = [
+  { id: "todo-0", name: "eat", completed: true },
+  { id: "todo-1", name: "sleep", completed: false },
+  { id: "todo-2", name: "repeat", completed: false },
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App tasks={DATA} />,
   document.getElementById('root')
 );
 
